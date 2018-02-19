e77d73a396fbbb89f3cc0a118a2460f7ad1b56d6
Juan Carlo <jcgcecilia@gmail.com>
2018-02-19 20:41:15 +0100

    Initial commit

---
+++ build/CMakeFiles/test_runner1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../test/test_runners/TestProductionCode_Runner.c"
+  "CMakeFiles/test_runner1.dir/src/ProductionCode.c.o"
+  "CMakeFiles/test_runner1.dir/test/TestProductionCode.c.o"
+  "CMakeFiles/test_runner1.dir/test/test_runners/TestProductionCode_Runner.c.o"
+  "bin/test_runner1.pdb"
+  "bin/test_runner1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_runner1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/unity/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jcc/Workspace/C/Unity/examples/cmake/example_1/unity
+# Build directory: /home/jcc/Workspace/C/Unity/examples/cmake/example_1/build/unity
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
